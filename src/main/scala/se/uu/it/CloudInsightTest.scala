addeb5ce9e59c0a6f390aa7d7dda564d7ae4ef66
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-11-29 15:08:05 +0100

    init eclipse project

---
+++ src/main/scala/se/uu/it/CloudInsightTest.scala
@@ -0,0 +1,11 @@
+package se.uu.it
+
+import org.junit.runner.RunWith
+import org.apache.spark.SharedSparkContext
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CloudInsightTest extends FunSuite with SharedSparkContext {
+
+}
---
+++ src/test/scala/se/uu/it/CoudInsight.scala
@@ -0,0 +1,5 @@
+package se.uu.it
+
+class CoudInsight {
+
+}
