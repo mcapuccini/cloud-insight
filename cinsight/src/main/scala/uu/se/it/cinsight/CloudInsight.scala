cea1cb86eb1d99c367b26f56bd1445d7fce10c30
Adrien Coulier <adrien.coulier@it.uu.se>
2016-12-12 16:49:55 +0100

    Add engine class documentation

--- cinsight/src/main/scala/uu/se/it/cinsight/CloudInsight.scala
+++ cinsight/src/main/scala/uu/se/it/cinsight/CloudInsight.scala
@@ -1,5 +1,24 @@
 package uu.se.it.cinsight
+import scala.collection.mutable.HashMap
 
-class CloudInsight {
-  
+/** Main engine
+  *
+  * @constructor create a new engine with parameters and model file
+  * @param prior prior distribution of the parameters
+  * @param beta confidence level of the test
+  * @param u number of particles per populations
+  * @param epsilon tolerance schedule
+  * @param model filename of the model
+  * @param data flow cytometry data set
+  */
+class CloudInsight
+    (
+        prior: (HashMap[String, Double]) => Double,
+        beta: Double,
+        u: Int,
+        epsilon: Seq[Double],
+        model: String,
+        data: Seq[(Double, Iterable[Double])])
+{
+
 }
