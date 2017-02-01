578bd5c36c9a68d5472ea918431dc25bfe82a8ea
Adrien Coulier <adrien.coulier@it.uu.se>
2017-02-01 15:37:30 +0100

    Autoformat code

--- cinsight/src/main/scala/uu/se/it/cinsight/CloudInsight.scala
+++ cinsight/src/main/scala/uu/se/it/cinsight/CloudInsight.scala
@@ -6,78 +6,81 @@ import scala.math.log
 import scala.math.pow
 import scala.math.sqrt
 
-/** Main engine
-  *
-  * @constructor create a new engine with parameters and model file
-  * @param prior prior distribution of the parameters
-  * @param beta confidence level of the test
-  * @param u number of particles per populations
-  * @param epsilon tolerance schedule
-  * @param model filename of the model
-  * @param data flow cytometry data set (sequence of pairs (time, cytometry data))
-  */
-class CloudInsight
-    (
-        prior: (HashMap[String, Double]) => Double,
-        beta: Double,
-        U: Int,
-        epsilon: Seq[Double],
-        model: String,
-        data: Seq[(Double, Iterable[Double])])
-{
-    val alpha = 1 - sqrt(1-beta)
-    val T = epsilon.length
-    val M = (for ((time, values) <- data) yield values.size).min
-    val S = for (eps <- epsilon) yield (
-            ceil(-log(alpha/2)
-                /(2*pow(eps-sqrt(-log(alpha/2)/(2*M)),2))).toInt
-            )
+/**
+ * Main engine
+ *
+ * @constructor create a new engine with parameters and model file
+ * @param prior prior distribution of the parameters
+ * @param beta confidence level of the test
+ * @param u number of particles per populations
+ * @param epsilon tolerance schedule
+ * @param model filename of the model
+ * @param data flow cytometry data set (sequence of pairs (time, cytometry data))
+ */
+class CloudInsight(
+    prior: (HashMap[String, Double]) => Double,
+    beta: Double,
+    U: Int,
+    epsilon: Seq[Double],
+    model: String,
+    data: Seq[(Double, Iterable[Double])]) {
+  val alpha = 1 - sqrt(1 - beta)
+  val T = epsilon.length
+  val M = (for ((time, values) <- data) yield values.size).min
+  val S = for (eps <- epsilon) yield (
+    ceil(-log(alpha / 2)
+      / (2 * pow(eps - sqrt(-log(alpha / 2) / (2 * M)), 2))).toInt)
 
-    var t = 1
+  var t = 1
 
-    /** Samples one candidate parameter point according to prior distribution
-      *     and previously accepted points.
-      *
-      * @return a new parameter point
-      */
-    def sample_candidate() : HashMap[String, Double] = {
-      return HashMap[String, Double]()
-    }
+  /**
+   * Samples one candidate parameter point according to prior distribution
+   *     and previously accepted points.
+   *
+   * @return a new parameter point
+   */
+  def sample_candidate(): HashMap[String, Double] = {
+    return HashMap[String, Double]()
+  }
 
-    /** Performs sc simulations with given parameter theta
-      *
-      * @param theta parameter set to use for these simulations
-      * @param sc number of simulations to perform
-      * @return simulated flow cytometry data according to theta
-      */
-    def run_simulation(theta: HashMap[String, Double], sc: Int) : Seq[Iterable[Double]] = {
-      return Seq[Iterable[Double]]()
-    }
+  /**
+   * Performs sc simulations with given parameter theta
+   *
+   * @param theta parameter set to use for these simulations
+   * @param sc number of simulations to perform
+   * @return simulated flow cytometry data according to theta
+   */
+  def run_simulation(theta: HashMap[String, Double], sc: Int): Seq[Iterable[Double]] = {
+    return Seq[Iterable[Double]]()
+  }
 
-    /** Measures Kolmogorov distance between two data sets
-      *
-      * Each datasets consists of a list of sets of values (one set per
-      * timepoint). x and y should have the same length.
-      *
-      * @param x first dataset
-      * @param y second dataset
-      * @return the distance between the two datasets
-      */
-    def kolmogorov_distance(x: Seq[Iterable[Double]], y: Seq[Iterable[Double]]) : Double = {
-      assert(x.length == y.length)
-      x.zip(y).map({case (a, b) =>
-                 (a.map((_, b.size)) ++ b.map((_, -a.size))) // Merge experimental and simulated datasets
-                 .groupBy(_._1).map({case (k, v) => (k, v.map(_._2).sum)}).toList // Aggregate duplicates
-                 .sortBy(_._1).map(_._2).scanLeft(0.0)(_ + _) // Compute cumulative sum
-                 .map(_.abs).max/(a.size*b.size)
-        }).max
-    }
+  /**
+   * Measures Kolmogorov distance between two data sets
+   *
+   * Each datasets consists of a list of sets of values (one set per
+   * timepoint). x and y should have the same length.
+   *
+   * @param x first dataset
+   * @param y second dataset
+   * @return the distance between the two datasets
+   */
+  def kolmogorov_distance(x: Seq[Iterable[Double]], y: Seq[Iterable[Double]]): Double = {
+    assert(x.length == y.length)
+    x.zip(y).map({
+      case (a, b) =>
+        (a.map((_, b.size)) ++ b.map((_, -a.size))) // Merge experimental and simulated datasets
+          .groupBy(_._1).map({ case (k, v) => (k, v.map(_._2).sum) }).toList // Aggregate duplicates
+          .sortBy(_._1).map(_._2).scanLeft(0.0)(_ + _) // Compute cumulative sum
+          .map(_.abs).max / (a.size * b.size)
+    }).max
+  }
 
-    /** Run the INSIGHT algorithm
-      *
-      * @return sampled posterior distribution of the parameter space
-      */
-    def run() : Seq[Iterable[HashMap[String, Double]]] = {
-      return Seq[Iterable[HashMap[String, Double]]]()
-    }
+  /**
+   * Run the INSIGHT algorithm
+   *
+   * @return sampled posterior distribution of the parameter space
+   */
+  def run(): Seq[Iterable[HashMap[String, Double]]] = {
+    return Seq[Iterable[HashMap[String, Double]]]()
+  }
 }
--- cinsight/src/test/scala/uu/se/it/cinsight/CloudInsightTest.scala
+++ cinsight/src/test/scala/uu/se/it/cinsight/CloudInsightTest.scala
@@ -12,43 +12,42 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class CloudInsightTest extends FunSuite with SharedSparkContext with BeforeAndAfter {
-  
-    var engine: CloudInsight = _
-    
-    before {
-        engine = new CloudInsight(
-            (x: HashMap[String, Double]) => 0.0,
-            0.05,
-            10,
-            List(0.1, 0.09, 0.06, 0.04),
-            "model.xml",
-            List((1.0, List.fill(1000)(List(0.0, 1.0, 2.0, 1.0, 0.5)).flatten),
-                 (2.0, List.fill(1000)(List(10.0, 11.0, 10.0, 12.0, 11.1)).flatten),
-                 (3.0, List.fill(1500)(List(20.0, 21.0, 30.0, 52.0, 12.3)).flatten))
-        )
-    }
-    test("Engine construction"){
-        assert(engine.alpha === 1-sqrt(1-0.05))
-        assert(engine.T === 4)
-        assert(engine.M === 5000)
-        assert(engine.S === List(350, 458, 1430, 5991))
 
-        assert(engine.t === 1)
-    }
-    test("Basic Kolmogorov distances are measured properly") {
-        assert(engine.kolmogorov_distance(List(List(0.0)), List(List(10.0))) === 1.0)
-        assert(engine.kolmogorov_distance(List(List(-10.0)), List(List(10.0))) === 1.0)
+  var engine: CloudInsight = _
 
-        assert(engine.kolmogorov_distance(List(List(0.0)), List(List(0.0))) === 0.0)
-        assert(engine.kolmogorov_distance(List(List(1.0)), List(List(1.0))) === 0.0)
-        assert(engine.kolmogorov_distance(List(List(-1.0)), List(List(-1.0))) === 0.0)
-        assert(engine.kolmogorov_distance(List(List(1.0, 0.0, -1)), List(List(0.0, -1, 1))) === 0.0)
-        assert(engine.kolmogorov_distance(List(List(1.0, -1)), List(List(0.0))) === 0.5)
-        assert(engine.kolmogorov_distance(List(List(1.0, 0.0)), List(List(-1.0))) === 1.0)
+  before {
+    engine = new CloudInsight(
+      (x: HashMap[String, Double]) => 0.0,
+      0.05,
+      10,
+      List(0.1, 0.09, 0.06, 0.04),
+      "model.xml",
+      List((1.0, List.fill(1000)(List(0.0, 1.0, 2.0, 1.0, 0.5)).flatten),
+        (2.0, List.fill(1000)(List(10.0, 11.0, 10.0, 12.0, 11.1)).flatten),
+        (3.0, List.fill(1500)(List(20.0, 21.0, 30.0, 52.0, 12.3)).flatten)))
+  }
+  test("Engine construction") {
+    assert(engine.alpha === 1 - sqrt(1 - 0.05))
+    assert(engine.T === 4)
+    assert(engine.M === 5000)
+    assert(engine.S === List(350, 458, 1430, 5991))
 
-        assert(engine.kolmogorov_distance(List(List(0.0), List(1.0)), List(List(10.0), List(1.0))) === 1.0)
-        assert(engine.kolmogorov_distance(List(List(0.0), List(1.0)), List(List(10.0), List(-1.0))) === 1.0)
+    assert(engine.t === 1)
+  }
+  test("Basic Kolmogorov distances are measured properly") {
+    assert(engine.kolmogorov_distance(List(List(0.0)), List(List(10.0))) === 1.0)
+    assert(engine.kolmogorov_distance(List(List(-10.0)), List(List(10.0))) === 1.0)
 
-        assert(engine.kolmogorov_distance(List(List(0.0), List(1.0)), List(List(0.0), List(1.0))) === 0.0)
-    }
+    assert(engine.kolmogorov_distance(List(List(0.0)), List(List(0.0))) === 0.0)
+    assert(engine.kolmogorov_distance(List(List(1.0)), List(List(1.0))) === 0.0)
+    assert(engine.kolmogorov_distance(List(List(-1.0)), List(List(-1.0))) === 0.0)
+    assert(engine.kolmogorov_distance(List(List(1.0, 0.0, -1)), List(List(0.0, -1, 1))) === 0.0)
+    assert(engine.kolmogorov_distance(List(List(1.0, -1)), List(List(0.0))) === 0.5)
+    assert(engine.kolmogorov_distance(List(List(1.0, 0.0)), List(List(-1.0))) === 1.0)
+
+    assert(engine.kolmogorov_distance(List(List(0.0), List(1.0)), List(List(10.0), List(1.0))) === 1.0)
+    assert(engine.kolmogorov_distance(List(List(0.0), List(1.0)), List(List(10.0), List(-1.0))) === 1.0)
+
+    assert(engine.kolmogorov_distance(List(List(0.0), List(1.0)), List(List(0.0), List(1.0))) === 0.0)
+  }
 }
