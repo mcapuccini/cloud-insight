95cfbf0163b1e84092987477954b070d52594fb6
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2017-05-11 10:25:56 +0200

    Add master param to run from eclipse

--- cinsight/src/main/scala/se/uu/it/cinsight/Main.scala
+++ cinsight/src/main/scala/se/uu/it/cinsight/Main.scala
@@ -2,19 +2,44 @@ package se.uu.it.cinsight
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import scopt.OptionParser
 
 object Main {
-  
+
+  case class Params(
+    master: String = null)
+
   def main(args: Array[String]) = {
-    
+
+    val defaultParams = Params()
+
+    val parser = new OptionParser[Params]("Main") {
+      opt[String]("master")
+        .text("spark master")
+        .action((x, c) => c.copy(master = x))
+    }
+
+    parser.parse(args, defaultParams).map { params =>
+      run(params)
+    } getOrElse {
+      sys.exit(1)
+    }
+
+  }
+
+  def run(params: Params) {
+
     val conf = new SparkConf()
       .setAppName("CloudINSIGHT")
+    if (params.master != null) {
+      conf.setMaster(params.master)
+    }
     val sc = new SparkContext(conf)
 
     // Experiment goes here
-    
+
     sc.stop
-    
+
   }
 
 }
